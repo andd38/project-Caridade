fa549b70694a805f474f22a5ce17ceee4f2fd5f6
andre patriarca <andrepatriarca4@gmail.com>
2024-01-27 20:06:46 -0300

    firstcommit

---
+++ src/main/java/com/example/projeto/CaridadeProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaridadeProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaridadeProjetoApplication.class, args);
+	}
+
+}
